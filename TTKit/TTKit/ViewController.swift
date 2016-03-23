953e49fd23fa35b66a9a9a322dfc6a17706b7bc1
陈荣雄 <rongxiong_chen@buestc.com>
2016-03-23 14:24:01 +0800

    Project initialed.

---
+++ TTKit/TTKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTKit
+//
+//  Created by 陈荣雄 on 3/23/16.
+//  Copyright © 2016 陈荣雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
